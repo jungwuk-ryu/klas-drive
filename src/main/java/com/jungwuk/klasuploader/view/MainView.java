3af2319ec3a50100ee828c62290e63993f7618f9
jungwuk <vojougae35@gmail.com>
2024-06-17 14:41:24 +0900

    design: 메인화면 레이아웃 변경

--- src/main/java/com/jungwuk/klasuploader/view/MainView.java
+++ src/main/java/com/jungwuk/klasuploader/view/MainView.java
@@ -14,10 +14,11 @@ public class MainView extends JFrame {
     private JButton uploadButton;
     @Getter
     private JTextField downloadPathField;
+    private JLabel titleLabel;
 
     public MainView() {
         setTitle("KLAS Drive");
-        setSize(400, 300);
+        setSize(400, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
@@ -29,13 +30,36 @@ public class MainView extends JFrame {
 
     private void initComponents() {
         uploadButton = new JButton("새 파일 업로드");
+
         downloadPathField = new JTextField();
+        downloadPathField.setEditable(true);
+
         fileList = new JList<>(listModel);
+
+        titleLabel = new JLabel("KLAS Drive");
+        titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, 20f));
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
     }
 
     private void layoutComponents() {
-        add(uploadButton, BorderLayout.NORTH);
-        add(new JScrollPane(fileList), BorderLayout.CENTER);
-        add(downloadPathField, BorderLayout.SOUTH);
+        JPanel parentPanel = new JPanel(new BorderLayout());
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout());
+        topPanel.add(uploadButton);
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new BorderLayout());
+
+        JScrollPane fileScrollPane = new JScrollPane(fileList);
+
+        centerPanel.add(downloadPathField, BorderLayout.NORTH);
+        centerPanel.add(fileScrollPane, BorderLayout.CENTER);
+
+        parentPanel.add(topPanel, BorderLayout.NORTH);
+        parentPanel.add(centerPanel, BorderLayout.CENTER);
+
+        add(titleLabel, BorderLayout.NORTH);
+        add(parentPanel, BorderLayout.CENTER);
     }
 }
